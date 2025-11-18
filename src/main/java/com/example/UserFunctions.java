e200cd7c4621f2d3a6544310ab70914272003d35
taketo-HW <hw.taketo@gmail.com>
2025-11-18 10:58:07 +0900

    4　基本演習問題の修正2

--- src/main/java/com/example/UserFunctions.java
+++ src/main/java/com/example/UserFunctions.java
@@ -55,100 +55,36 @@ public class UserFunctions {
 
     /**
      * 年齢、性別、体力テストの点数を考慮して体力テストの合格判定を行う。
-     * 複雑な条件分岐の入れ子構造で判定ロジックを実装。
+     * 3階層の入れ子構造で4つのパターンに分けて判定ロジックを実装。
      * @return 体力テストに合格した場合true
      */
     public boolean isFitnessTestPassed() {
-        // 基本的な年齢チェック
-        if (this.age < 18) {
-            return false;
-        }
-        
-        // 年齢が18歳以上の場合、性別による条件分岐
+        // 第1階層: 20歳以上の場合
         if (this.age >= 20) {
-            // 20歳以上の場合
-            if (this.gender != null) {
-                if (this.gender.equals("MALE")) {
-                    // 男性の場合、20歳以上で点数が80点以上なら特別に合格
-                    if (this.fitnessScore >= 80) {
-                        return true;
-                    } else if (this.fitnessScore >= 60) {
-                        // 点数が60点以上80点未満なら合格
-                        return true;
-                    } else {
-                        return false;
-                    }
-                } else if (this.gender.equals("FEMALE")) {
-                    // 女性の場合、20歳以上で点数が75点以上なら特別に合格
-                    if (this.fitnessScore >= 75) {
-                        return true;
-                    } else if (this.fitnessScore >= 55) {
-                        // 点数が55点以上75点未満なら合格
-                        return true;
-                    } else {
-                        return false;
-                    }
-                } else {
-                    // その他の性別の場合、20歳以上で点数が78点以上なら特別に合格
-                    if (this.fitnessScore >= 78) {
-                        return true;
-                    } else if (this.fitnessScore >= 58) {
-                        // 点数が58点以上78点未満なら合格
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
-            } else {
-                // 性別が設定されていない場合、20歳以上で点数が65点以上が必要
-                if (this.fitnessScore >= 65) {
+            // 第2階層: 性別による分岐
+            if ("MALE".equals(this.gender)) {
+                // 第3階層: パターン1 - 20歳以上の男性
+                if (this.fitnessScore >= 60) {
                     return true;
                 } else {
                     return false;
                 }
+            } else if ("FEMALE".equals(this.gender)) {
+                // 第3階層: パターン2 - 20歳以上の女性
+                if (this.fitnessScore >= 55) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                // 第3階層: パターン3 - 20歳以上のその他/未設定
+                return false;
             }
         } else {
-            // 18歳以上20歳未満の場合
-            if (this.gender != null) {
-                if (this.gender.equals("FEMALE")) {
-                    // 女性は18-19歳の場合、点数が85点以上なら特別に合格
-                    if (this.fitnessScore >= 85) {
-                        return true;
-                    } else if (this.fitnessScore >= 70) {
-                        // 点数が70点以上85点未満なら合格
-                        return true;
-                    } else {
-                        return false;
-                    }
-                } else if (this.gender.equals("MALE")) {
-                    // 男性は18-19歳の場合、点数が90点以上なら特別に合格
-                    if (this.fitnessScore >= 90) {
-                        return true;
-                    } else if (this.fitnessScore >= 75) {
-                        // 点数が75点以上90点未満なら合格
-                        return true;
-                    } else {
-                        return false;
-                    }
-                } else {
-                    // その他の性別の場合、18-19歳で点数が88点以上なら特別に合格
-                    if (this.fitnessScore >= 88) {
-                        return true;
-                    } else if (this.fitnessScore >= 72) {
-                        // 点数が72点以上88点未満なら合格
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
-            } else {
-                // 性別が設定されていない場合、18-19歳は点数が80点以上でないと認めない
-                if (this.fitnessScore >= 80) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
+            // 第1階層: 19歳以下の場合
+            // 第2階層: 性別による分岐なし、一律で判定
+            // 第3階層: パターン4 - 19歳以下は一律80点以上で合格
+            return this.fitnessScore >= 80;
         }
     }
 }
