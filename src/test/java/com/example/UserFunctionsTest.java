5f9f753bfee6f4dd2d1e33ad2db4bb53c852b396
taketo-HW <hw.taketo@gmail.com>
2025-11-18 15:41:46 +0900

    4基本演習,回答例

--- src/test/java/com/example/UserFunctionsTest.java
+++ src/test/java/com/example/UserFunctionsTest.java
@@ -18,29 +18,28 @@ public class UserFunctionsTest {
         UserFunctions = new UserFunctions();
     }
 
-    @Test //成人判定ロジックの検証（アサーション）
-    void testIsFitnessTestA() {
+    @Test // 成人判定ロジックの検証（アサーション）
+    void testIsMaleFitnessTestPassed_1A_1() {
         UserFunctions.setAge(20);
         UserFunctions.setGender("MALE");
         UserFunctions.setFitnessScore(60);
         assertTrue(UserFunctions.isFitnessTestPassed(), "テストケース A: 20歳以上男性の合格テスト（最小合格境界値）");
     }
-    
 
-    @Test //体力テスト女性不合格ロジックの検証（アサーション）
-    void testIsFitnessTestB() {
+    @Test //成人判定ロジックの検証（アサーション）
+    void testIsMaleFitnessTestPassed_1A_2() {
         UserFunctions.setAge(19);
-        UserFunctions.setGender("FEMALE");
+        UserFunctions.setGender("OTHER");
         UserFunctions.setFitnessScore(79);
-        assertFalse(UserFunctions.isFitnessTestPassed(), "テストケース B: 19歳以下の不合格テスト（最大不合格境界値）");
+        assertFalse(UserFunctions.isFitnessTestPassed(), "19歳以下の不合格テスト（最大不合格境界値）");
     }
 
-    @Test //体力テスト女性不合格ロジックの検証（アサーション）
-    void testIsFitnessTestC() {
+    @Test //成人判定ロジックの検証（アサーション）
+    void testIsFemaleFitnessTestPassed_1A_3() {
         UserFunctions.setAge(25);
         UserFunctions.setGender("FEMALE");
         UserFunctions.setFitnessScore(54);
-        assertFalse(UserFunctions.isFitnessTestPassed(), "テストケース C: 20歳以上女性の不合格境界値テスト（最大不合格境界値）");
+        assertFalse(UserFunctions.isFitnessTestPassed(), "20歳以上女性の不合格境界値テスト（最大不合格境界値）");
     }
 
     @AfterEach
